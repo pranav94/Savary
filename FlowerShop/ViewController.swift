fcb75ae3439e1495e0cea59a73e9388ef27d7c87
Pranav Udupa S <pranavudupa94@gmail.com>
2019-11-24 22:15:08 -0800

    Savary initial Image Recognition Commit

---
+++ FlowerShop/ViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  FlowerShop
+//
+//  Created by Brian Advent on 14.06.18.
+//  Copyright © 2018 Brian Advent. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/GameScene.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // Image Detection
+        configuration.detectionImages = ARReferenceImage.referenceImages(inGroupNamed: "FlowerObjects", bundle: Bundle.main)!
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
+        let node = SCNNode()
+        
+        if let imageAnchor = anchor as? ARImageAnchor {
+            let plane = SCNPlane(width: 0.6, height: 0.3)
+            
+            plane.cornerRadius = plane.width / 32
+            
+            let spriteKitScene = SKScene(fileNamed: "ProductInfo")
+            
+            plane.firstMaterial?.diffuse.contents = spriteKitScene
+            plane.firstMaterial?.isDoubleSided = true
+            plane.firstMaterial?.diffuse.contentsTransform = SCNMatrix4Translate(SCNMatrix4MakeScale(1, -1, 1), 0, 1, 0)
+            
+            let planeNode = SCNNode(geometry: plane)
+//            planeNode.position = SCNVector3Make(imageAnchor.referenceImage.center.x, imageAnchor.referenceImage.center.y + 0.35, imageAnchor.referenceImage.center.z)
+            
+            planeNode.eulerAngles.x = -.pi/2
+            
+            let labelNode = spriteKitScene?.childNode(withName: "label") as? SKLabelNode
+            labelNode?.text = imageAnchor.referenceImage.name
+
+            
+            node.addChildNode(planeNode)
+            
+        }
+        
+        return node
+    }
+    
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
